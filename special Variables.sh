ed35c32f965de7fd39804bb972f5e4d26421ba97
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-22 10:42:07 +0530

    Add New Things

---
+++ special Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Print all variable :"$@
+echo "Print No of variables Passed:"$#
+echo "Script Name:"$0
+echo "Print current working Directory:"$PWD
+echo "Print Home directory of current User :"$Home
+echo "Print Which user is running this script:"$User
+echo "Print Host Name:"$HOSTNAME
+echo "Print Process id of current script:"$$
