cf4ffefe9d856b01b0bc4a807c733e2c382493aa
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-22 12:19:21 +0530

    Add New Things

--- installation.sh
+++ installation.sh
@@ -4,27 +4,25 @@ userid=$(id -u)
 
 if [ $userid -ne 0 ]; then
   echo "Please run the script with root access"
-  exit 1 # Manually exit if error comes
+  exit 1
 else
   echo "You are super user"
 fi
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]
-then
-echo "Installation of mysql is failure"
-exit 1
+if [ $? -ne 0 ]; then
+  echo "Installation of mysql is failure"
+  exit 1
 fi
 
 dnf install git -y
 
-if [ $? -ne 0 ]
-then 
-echo "Installation of git is Failure"
-exit 1
+if [ $? -ne 0 ]; then 
+  echo "Installation of git is Failure"
+  exit 1
 else
-echo "installation of git is success"
+  echo "installation of git is success"
 fi
 
-echo "is script proceeding?
+echo "is script proceeding?"
