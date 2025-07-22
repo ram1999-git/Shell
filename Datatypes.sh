581b0aeb9e7bc46335058dc304414bcf9d9d7207
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-22 10:22:47 +0530

    Add New Things

--- Datatypes.sh
+++ Datatypes.sh
@@ -4,6 +4,6 @@
 
 Names=("Ram" "Raj" "Sirish")
 
-echo "Print First Name is $(Name[0])"
-echo "Print Last Name is $(Name[2])"
-echo "Print All Names are $(Name[@])"
+echo "Print First Name is :${Name[0]}"
+echo "Print Last Name is  :${Name[2]}"
+echo "Print All Names are :${Name[@]}"
