974e706ef975857d6ce778c20a6bdbc93babbf76
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-22 17:15:43 +0530

    This functions

--- colours.sh
+++ colours.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
-
 Timestamp=$(date +%F-%H-%M-%S)
 Script_Name=$(echo $0 | cut -d "." -f1)
 Log_file=/temp/$Script_Name-$Timestamp.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "script started executing at :$Timestamp"
 
 install_package() {
   if [ "$(id -u)" -ne 0 ]; then
@@ -12,9 +15,9 @@ install_package() {
 
   dnf install $1 -y
   if [ $? -eq 0 ]; then
-    echo "$1 installed successfully"
+    echo -e "$1 $G installed successfully"
   else
-    echo "Failed to install $1"
+    echo -e "$R Failed to install $1"
     exit 1
   fi
 }
