e0dcffaa04246ace3b17ae864904a24503facec1
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-22 15:22:53 +0530

    This functions

--- Functions.sh
+++ Functions.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
 install_package() {
+  if [ "$(id -u)" -ne 0 ]; then
+    echo "Error: This command has to be run with superuser privileges"
+    exit 1
+  fi
+
   dnf install $1 -y
   if [ $? -eq 0 ]; then
     echo "$1 installed successfully"
@@ -8,4 +15,5 @@ install_package() {
   fi
 }
 
-
+# Call the function
+install_package $1
