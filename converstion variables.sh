a4cf526b3cb7ef73916141bb22610d117ac30844
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-22 10:04:17 +0530

    This is a Test Class of Shell

---
+++ converstion variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=Ramesh
+Person2=Suresh
+
+echo "$Person1::Hi $Person2, How are You"
+echo "$Person2::Hi, I'm good What about You"
+echo "$Person1:: What are you doing currently"
+echo "$Person2:: I'm on software Industry learning DevOps"
+
